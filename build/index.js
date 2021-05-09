877c0c3f17446d0b133db7117e37a1c3243914fd
laoma <laoma@tuocad.com>
2021-05-10 01:14:17 +0800

    修改bug

--- build/index.js
+++ build/index.js
@@ -1,27 +1,11 @@
 "use strict";
 
-require("core-js/modules/es6.object.assign.js");
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-require("core-js/modules/es6.weak-map.js");
-
-require("core-js/modules/es6.string.iterator.js");
-
-require("core-js/modules/es6.object.to-string.js");
-
-require("core-js/modules/es6.array.iterator.js");
-
-require("core-js/modules/web.dom.iterable.js");
-
-require("core-js/modules/es6.object.get-own-property-descriptor.js");
-
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 exports.withWatcher = exports.default = void 0;
-
-require("core-js/modules/es6.array.find-index.js");
-
-require("core-js/modules/es6.regexp.match.js");
-
-require("core-js/modules/es6.object.set-prototype-of.js");
 
 var _react = _interopRequireWildcard(require("react"));
 
@@ -29,75 +13,100 @@ var _reactRouter = require("react-router");
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _inheritsLoose(subClass, superClass) { subClass.prototype = Object.create(superClass.prototype); subClass.prototype.constructor = subClass; _setPrototypeOf(subClass, superClass); }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 var ReactRouterWatcher = /*#__PURE__*/function (_Component) {
-  _inheritsLoose(ReactRouterWatcher, _Component);
+  _inherits(ReactRouterWatcher, _Component);
+
+  var _super = _createSuper(ReactRouterWatcher);
 
   function ReactRouterWatcher() {
     var _this;
 
-    _this = _Component.call(this) || this;
+    _classCallCheck(this, ReactRouterWatcher);
+
+    _this = _super.call(this);
     _this.listeners = [];
     _this.addRouteChangeListener = _this.addRouteChangeListener.bind(_assertThisInitialized(_this));
     _this.removeRouteChangeListener = _this.removeRouteChangeListener.bind(_assertThisInitialized(_this));
     return _this;
   }
 
-  var _proto = ReactRouterWatcher.prototype;
-
-  _proto.addRouteChangeListener = function addRouteChangeListener(listener) {
-    this.listeners.push(listener);
-  };
-
-  _proto.removeRouteChangeListener = function removeRouteChangeListener(listener) {
-    var index = this.listeners.findIndex(function (item) {
-      return item === listener;
-    });
-
-    if (index >= 0) {
-      this.listeners.splice(index, 1);
+  _createClass(ReactRouterWatcher, [{
+    key: "addRouteChangeListener",
+    value: function addRouteChangeListener(listener) {
+      this.listeners.push(listener);
     }
-  };
-
-  _proto.componentDidUpdate = function componentDidUpdate() {
-    this.execListeners();
-  };
-
-  _proto.componentDidMount = function componentDidMount() {
-    this.execListeners();
-  };
-
-  _proto.execListeners = function execListeners() {
-    var _this2 = this;
-
-    this.listeners.forEach(function (func) {
-      func({
-        history: _this2.props.history,
-        match: _this2.props.match,
-        location: _this2.props.location
+  }, {
+    key: "removeRouteChangeListener",
+    value: function removeRouteChangeListener(listener) {
+      var index = this.listeners.findIndex(function (item) {
+        return item === listener;
       });
-    });
-  };
 
-  _proto.shouldComponentUpdate = function shouldComponentUpdate(nextProps) {
-    return nextProps.location.pathname !== this.props.location.pathname;
-  };
+      if (index >= 0) {
+        this.listeners.splice(index, 1);
+      }
+    }
+  }, {
+    key: "componentDidUpdate",
+    value: function componentDidUpdate() {
+      this.execListeners();
+    }
+  }, {
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.execListeners();
+    }
+  }, {
+    key: "execListeners",
+    value: function execListeners() {
+      var _this2 = this;
 
-  _proto.render = function render() {
-    return this.props.children({
-      addRouteChangeListener: this.addRouteChangeListener,
-      removeRouteChangeListener: this.removeRouteChangeListener
-    });
-  };
+      this.listeners.forEach(function (func) {
+        func({
+          history: _this2.props.history,
+          match: _this2.props.match,
+          location: _this2.props.location
+        });
+      });
+    }
+  }, {
+    key: "shouldComponentUpdate",
+    value: function shouldComponentUpdate(nextProps) {
+      return nextProps.location.pathname !== this.props.location.pathname;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return this.props.children({
+        addRouteChangeListener: this.addRouteChangeListener,
+        removeRouteChangeListener: this.removeRouteChangeListener
+      });
+    }
+  }]);
 
   return ReactRouterWatcher;
 }(_react.Component);
